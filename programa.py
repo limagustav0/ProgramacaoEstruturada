18079f32aaced7f873be7023356ba456083e8a0a
parallax <gustavasd@gmail.com>
2022-03-23 19:53:46 -0300

    manutenção do código

--- programa.py
+++ programa.py
@@ -1,53 +1,34 @@
-import random
+def encriptador(texto):
+    import random
+    import string
+    letra = string.ascii_letters.lower() + string.ascii_letters.upper() + '!@#$%¨&*1234567890'
+    try:
+        liberador = [random.choice(letra) for x in range(0,4)]
+        liberador = (''.join(liberador))
 
+        textoCriptografado = [random.choice(letra) for i in range(0,len(texto))]
 
-letra = ['1','2','3','4','5','6','7','8','9','0','¥','$','&','!','?','@','€','a','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r',
-         's','t','u','v','w','x','y','z',]
-a = random.choice(letra)
-b = random.choice(letra)
-c = random.choice(letra)
-d = random.choice(letra)
-e = random.choice(letra)
-crip = []
-pin = [a,b,c,d,e]
-encrip = []
-text = str(input("▶ Digite o texto: "))
-crt = len(text)
-cont = 0
+        textoCriptografado = (''.join(textoCriptografado))
 
-for x in text:
-    crip.append(text[cont])
-    cont +=1
-for y in range(0,crt):
-    encrip.append(random.choice(letra))
-print('A criptografia de {} é: '.format(text))
-for z in range(0,crt):
-    print(encrip[z], end='')
-cripto = (''.join(encrip))
-pim = (''.join(pin))
-print("\n\n✊ PIN: {}".format(pim))
-pintest = input("Digite o PIN para liberar o decodificador: ")
-if pintest == pim:
-    desc = input("\n✋ Decodificador liberado. \nDescriptografe o código com o texto gerado: ")
-else:
-    while pim != pintest:
-        print("PIN incorreto. O PIN será redefinido.")
-        a = random.choice(letra)
-        b = random.choice(letra)
-        c = random.choice(letra)
-        d = random.choice(letra)
-        e = random.choice(letra)
-        pin = [a,b,c,d,e]
-        pim = (''.join(pin))
-        print('\n✊ PIN:', pim)
+        print(f'A criptografia do texto digitado é {texto} é {textoCriptografado}')
+
+        print("\n\n✊ PIN >>> {}".format(liberador))
         pintest = input("Digite o PIN para liberar o decodificador: ")
-        if pintest == pim:
-            desc = input("\n✋ Decodificador liberado. \nDescriptografe o código com o texto gerado: ")
-if desc == cripto:
-    print('\n✅ Código descriptografado. O texto é {}.'.format(text))
-else:
-    while desc != cripto:
-        print("Errou ❗ Escreva a codificação certa!")
-        desc = input("Descript: ")
-        if desc == cripto:
-            print('\n✅ Código descriptografado. O texto é {}.'.format(text))
+
+        while liberador != pintest:
+            print("PIN incorreto. O PIN será redefinido.")
+            liberador = [random.choice(letra) for x in range(0, 4)]
+            liberador = (''.join(liberador))
+            print('\n✊ PIN:', liberador)
+            pintest = input("Digite o PIN para liberar o decodificador: ")
+        else:
+            decriptador = input("\n✋ Decodificador liberado. \nDescriptografe o código com a criptografia gerada: ")
+            while decriptador != textoCriptografado:
+                decriptador = input("Entrada inválida❗ Escreva a codificação certa! \nDescriptografe o código com a criptografia gerada: ")
+            else:
+                print('\n✅ Texto descriptografado. O texto digitado é {}.'.format(texto))
+    except Exception as e:
+        print(e)
+
+if __name__ == '__main__':
+    encriptador('1234567')
